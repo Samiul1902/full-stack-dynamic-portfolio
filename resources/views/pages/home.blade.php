4c02b3bc6717171906055e70159aa74adffffaf0
samiul1902 <sakib22205101472@diu.edu.bd>
2025-10-18 15:24:51 +0600

    first commit

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main')
+@section("content")
+<h1>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium non dicta recusandae reiciendis voluptatibus tempora mollitia, ullam minus eos eligendi! Molestias, illo, quae obcaecati rem porro aliquid impedit est quibusdam autem nemo ad itaque commodi deleniti ut? Accusantium, ab quae earum placeat tenetur impedit fugit aut optio obcaecati iusto corporis eum in velit excepturi sunt dolorum iste voluptatem aliquam? Debitis eligendi, necessitatibus exercitationem impedit rem eius possimus unde illum asperiores sequi incidunt maiores obcaecati cupiditate nobis quod vel nesciunt beatae sit ad mollitia nihil quisquam. Odit repellendus aut cum eaque perferendis, quae reprehenderit ratione quis neque itaque unde consequuntur vero?
+</h1>
+@endsection
