0d9997387d548db329232b204f4914f45bf96c10
samiul1902 <sakib22205101472@diu.edu.bd>
2025-12-02 20:14:53 +0600

    icon

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="/images/481773494_3447077962266966_1117281271806353893_n.jpg">
     <title>My Portfolio</title>
 </head>
 <body>
