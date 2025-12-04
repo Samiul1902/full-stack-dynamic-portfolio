a1ac2ead1032c9a92e4307dfe5f41840421a6ad8
samiul1902 <sakib22205101472@diu.edu.bd>
2025-12-04 20:58:07 +0600

    chore: remove unused project and skills sections from views

--- resources/views/pages/project.blade.php
+++
@@ -1,5 +0,0 @@
-<h2>Projects</h2>
-        <p>
-            Briefly describe a few key projects, linking to GitHub or live demos,
-            and mention what tech stack you used for each one.
-        </p>
--- resources/views/pages/skills.blade.php
+++
@@ -1,5 +0,0 @@
-<h2>Skills</h2>
-        <p>
-            Add your main technologies here, for example: HTML, CSS, JavaScript, Laravel, PHP,
-            MySQL, REST APIs, and tools you frequently use.
-        </p>
